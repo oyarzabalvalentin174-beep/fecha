37bf77fba6523e08142a6412c1b12688ef1c2be7
valentin <oyarzabalvalentin174@gmail.com>
2026-05-04 16:41:31 -0300

    clase fecha

---
+++ Fecha.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class Fecha here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Fecha
+{
+    // instance variables - replace the example below with your own
+    private int anho;
+    private int mes;
+    private int dia;
+    
+    
+}
